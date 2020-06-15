eed7f0f58ec2754becc0e3e5b01a433ad1738e9e
yehudamarc <yehudamarc@gmail.com>
2020-06-15 21:23:45 +0300

    tests. not compiled

--- ass3Tests.d
+++
@@ -1,2 +0,0 @@
-ass3Tests.o: ass3Tests.c /usr/include/stdc-predef.h types.h stat.h user.h \
- syscall.h
--- ass3Tests2.d
+++
@@ -1,2 +0,0 @@
-ass3Tests2.o: ass3Tests2.c /usr/include/stdc-predef.h types.h stat.h \
- user.h syscall.h
