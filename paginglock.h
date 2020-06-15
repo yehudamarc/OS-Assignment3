7aec6bac9e7701fe8349d5d1275104b770fe4bb2
yehudamarc <yehudamarc@gmail.com>
2020-06-15 17:08:02 +0300

    deleted paginglock files

--- paginglock.h
+++
@@ -1,9 +0,0 @@
-// Long-term locks for processes
-struct paginglock {
-  uint locked;       // Is the lock held?
-  struct spinlock lk; // spinlock protecting this sleep lock
-  
-  // For debugging:
-  char *name;        // Name of lock.
-  int pid;           // Process holding lock
-};
